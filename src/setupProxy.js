e55a9f6eff2478af43053cc5c879e7a5e08dcb59
yongtaecheon <ide2143@naver.com>
2024-03-25 22:57:31 +0900

    setupProxy updated

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
   app.use(
     createProxyMiddleware('/api', { //user api server
-      target: 'http://127.0.0.1:8080',
+      target: 'http://127.0.0.1:5000',
       changeOrigin: true,
     }),
     createProxyMiddleware('/dashboard', { //dashboard server
